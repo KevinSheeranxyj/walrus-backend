1d140e403415e4bd3fab22ba09f8a7e601ed653b
kevin sheeran <kevinsheeranxyj@gmail.com>
2024-09-01 12:02:36 +0800

    UPDATE: change to fixed host and port

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	"kevinsheeran/walrus-backend/config"
 	"kevinsheeran/walrus-backend/router"
-	"log"
 )
 
 // @title Survey
@@ -18,18 +14,18 @@ func main() {
 
 	//global.Log = core.InitLogger()
 	// Read the YAML file
-	data, err := ioutil.ReadFile("config.yaml")
-	if err != nil {
-		log.Fatalf("Error reading YAML file: %s", err)
-	}
-
-	// Unmarshal the YAML data into the Config struct
-	err = yaml.Unmarshal(data, &config.Config)
-	if err != nil {
-		log.Fatalf("Error parsing YAML file: %s", err)
-	}
+	//data, err := ioutil.ReadFile("config.yaml")
+	//if err != nil {
+	//	log.Fatalf("Error reading YAML file: %s", err)
+	//}
+	//
+	//// Unmarshal the YAML data into the Config struct
+	//err = yaml.Unmarshal(data, &config.Config)
+	//if err != nil {
+	//	log.Fatalf("Error parsing YAML file: %s", err)
+	//}
 	router := router.InitRouter()
 
-	router.Run(config.Config.System.Host + ":" + config.Config.System.Port)
+	router.Run("0.0.0.0:31411")
 
 }
